567637f367d561e5199e167ad4d3caafab9b2cef
Roberto <roberto.menesantos@gmail.com>
2024-11-16 09:58:52 -0300

    adding the spring validations on the entities fields

--- src/main/java/com/mai/solar/energyControl/models/Farm.java
+++ src/main/java/com/mai/solar/energyControl/models/Farm.java
@@ -1,9 +1,13 @@
 package com.mai.solar.energyControl.models;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 import lombok.Getter;
 import lombok.Setter;
+import org.springframework.format.annotation.DateTimeFormat;
 
 import java.time.LocalDate;
 
@@ -17,9 +21,14 @@ public class Farm {
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
     private String id;
+    @Size(min = 3, max = 55, message = "The \"name\" length needs to be between 3 and 55")
+    @NotEmpty(message = "The \"name\" cannot be null or blank")
     private String name;
+    @DateTimeFormat(pattern = "yyyy/MM/dd")
     private LocalDate subscriptionDate = LocalDate.now();
+    @Min(0)
     private Double hectareSize;
+    @Min(0)
     private Integer panelCount;
 
 }
