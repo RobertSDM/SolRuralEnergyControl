0e77ea82f1ae4a518e36ad169f795aa2870439f7
Roberto Menezes <129512414+RobertSDM@users.noreply.github.com>
2024-11-20 09:06:37 -0300

    connecting the messaging service with the database, now we can save EnergyHistory by messaging service

--- src/main/java/com/mai/solar/energyControl/services/EnergyHistoryService.java
+++ src/main/java/com/mai/solar/energyControl/services/EnergyHistoryService.java
@@ -1,7 +1,15 @@
 package com.mai.solar.energyControl.services;
 
 import com.mai.solar.energyControl.models.EnergyHistory;
+import com.mai.solar.energyControl.models.EnergyLevelType;
+import com.mai.solar.energyControl.models.Farm;
+import com.mai.solar.energyControl.models.SolarPanel;
+import com.mai.solar.energyControl.models.dto.EnergyHistoryHiveMQDTO;
+import com.mai.solar.energyControl.models.enums.EnergyType;
 import com.mai.solar.energyControl.repository.EnergyHistoryRepository;
+import com.mai.solar.energyControl.repository.EnergyLevelTypeRepository;
+import com.mai.solar.energyControl.repository.FarmRepository;
+import com.mai.solar.energyControl.repository.SolarPanelRepository;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -11,9 +19,15 @@ import java.util.Optional;
 public class EnergyHistoryService {
 
     private final EnergyHistoryRepository energyHistoryRep;
+    private final FarmRepository farmRep;
+    private final SolarPanelRepository panelRep;
+    private final EnergyLevelTypeRepository energyLevelTypeRep;
 
-    public EnergyHistoryService(EnergyHistoryRepository energyHistoryRep) {
+    public EnergyHistoryService(EnergyHistoryRepository energyHistoryRep, FarmRepository farmRep, SolarPanelRepository panelRep, EnergyLevelTypeRepository energyLevelTypeRep) {
         this.energyHistoryRep = energyHistoryRep;
+        this.farmRep = farmRep;
+        this.panelRep = panelRep;
+        this.energyLevelTypeRep = energyLevelTypeRep;
     }
 
     public List<EnergyHistory> getAll(){
@@ -22,6 +36,41 @@ public class EnergyHistoryService {
 
     public Optional<EnergyHistory> getById(Long id){
         return this.energyHistoryRep.findById(id);
+    }
+
+    public EnergyHistory save(EnergyHistory energyHistory){
+        return this.energyHistoryRep.save(energyHistory);
+    }
+
+    public EnergyHistory create(String panelId, String farmId, Double energyValue) throws Exception {
+        EnergyHistory energyHistory = new EnergyHistory();
+        EnergyLevelType energyLevelType;
+
+        Optional<Farm> farmOpt = farmRep.findById(farmId);
+        Optional<SolarPanel> panelOpt = panelRep.findById(panelId);
+
+        if (farmOpt.isEmpty() || panelOpt.isEmpty()) {
+            throw new Exception("Farm not found");
+        }
+
+        Farm farm = farmOpt.get();
+        SolarPanel panel = panelOpt.get();
+
+        energyHistory.setFarmId(farm);
+        energyHistory.setPanelId(panel);
+        energyHistory.setEnergyValue(energyValue);
+
+        if(energyValue < 10){
+            energyLevelType = energyLevelTypeRep.findById(EnergyType.LOW).orElse(null);
+        }else if(energyValue < 50){
+            energyLevelType = energyLevelTypeRep.findById(EnergyType.MEDIUM).orElse(null);
+        }else {
+            energyLevelType = energyLevelTypeRep.findById(EnergyType.HIGH).orElse(null);
+        }
+
+        energyHistory.setLevelTypeId(energyLevelType);
+
+        return energyHistory;
     }
 
 }
--- src/main/java/com/mai/solar/energyControl/services/HiveMQSubscriberSolarDataService.java
+++ src/main/java/com/mai/solar/energyControl/services/HiveMQSubscriberSolarDataService.java
@@ -1,7 +1,10 @@
 package com.mai.solar.energyControl.services;
 
+import com.google.gson.Gson;
 import com.hivemq.client.mqtt.mqtt3.message.publish.Mqtt3Publish;
 import com.mai.solar.energyControl.managers.HiveMqConnectionManager;
+import com.mai.solar.energyControl.models.EnergyHistory;
+import com.mai.solar.energyControl.models.dto.EnergyHistoryHiveMQDTO;
 import com.mai.solar.energyControl.models.interfaces.HiveMQSubscriber;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.event.ContextRefreshedEvent;
@@ -18,31 +21,45 @@ import java.time.format.DateTimeFormatter;
 public class HiveMQSubscriberSolarDataService implements HiveMQSubscriber {
 
     private final HiveMqConnectionManager hiveMqConnectionManager;
+    private final Gson gson;
+    private final EnergyHistoryService energyHistoryService;
+
 
     @Value("${hivemq.subscriber.solar-data.topic}")
     private String subscriberTopic;
 
-
-    public HiveMQSubscriberSolarDataService(HiveMqConnectionManager hiveMqConnectionManager) {
+    public HiveMQSubscriberSolarDataService(HiveMqConnectionManager hiveMqConnectionManager, Gson gson, EnergyHistoryService energyHistoryService) {
         this.hiveMqConnectionManager = hiveMqConnectionManager;
+        this.gson = gson;
+        this.energyHistoryService = energyHistoryService;
     }
 
     @EventListener(ContextRefreshedEvent.class)
     @Order(2)
-    public void connect(){
+    public void connect() {
         hiveMqConnectionManager.subscriber(subscriberTopic, this::messageHandler);
     }
 
-    public void messageHandler(Mqtt3Publish publish){
-        if(publish.getPayload().isPresent()){
+    public void messageHandler(Mqtt3Publish publish) {
+        if (publish.getPayload().isEmpty()) {
+            return;
+        }
 
-            DateTimeFormatter dateFormatter = DateTimeFormatter.ofPattern("yyyy/MM/dd - HH:mm:ss");
-            LocalDateTime dateTime = LocalDateTime.now();
+        DateTimeFormatter dateFormatter = DateTimeFormatter.ofPattern("yyyy/MM/dd - HH:mm:ss");
+        LocalDateTime dateTime = LocalDateTime.now();
 
-            System.out.println();
-            System.out.println(dateFormatter.format(dateTime) + ": " + "Data received on topic: " + publish.getTopic());
-            ByteBuffer payload = publish.getPayload().get();
-            String data = StandardCharsets.UTF_8.decode(payload).toString();
+        System.out.println();
+        System.out.println(dateFormatter.format(dateTime) + ": " + "Data received on topic: " + publish.getTopic());
+        ByteBuffer payload = publish.getPayload().get();
+        String dataString = StandardCharsets.UTF_8.decode(payload).toString();
+
+        try {
+            EnergyHistoryHiveMQDTO data = gson.fromJson(dataString, EnergyHistoryHiveMQDTO.class);
+            EnergyHistory energyHistory = energyHistoryService.create(data.panelId(), data.farmId(), data.energyValue());
+
+            energyHistoryService.save(energyHistory);
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
         }
     }
 
