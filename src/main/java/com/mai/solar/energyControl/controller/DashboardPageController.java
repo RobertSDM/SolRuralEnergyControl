63f33de2d2ebd99af7cb0cdbbf174971c29dffe4
Roberto <roberto.menesantos@gmail.com>
2024-11-20 21:08:12 -0300

    creating the dashboard page

---
+++ src/main/java/com/mai/solar/energyControl/controller/DashboardPageController.java
@@ -0,0 +1,45 @@
+package com.mai.solar.energyControl.controller;
+
+import com.mai.solar.energyControl.models.Farm;
+import com.mai.solar.energyControl.services.FarmService;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.List;
+
+@Controller
+@RequestMapping
+public class DashboardPageController {
+
+    private final FarmService farmService;
+
+    @Value("${pagination.default.size}")
+    private Integer defaultSize;
+
+    public DashboardPageController(FarmService farmService) {
+        this.farmService = farmService;
+    }
+
+    @GetMapping("/dashboard")
+    public ModelAndView dashboard(
+            @RequestParam(name = "page", defaultValue = "0") Integer page
+    ) {
+        ModelAndView mv = new ModelAndView("dashboard");
+        Pageable pageable = PageRequest.of(page, defaultSize);
+
+        Page<Farm> farmsPage = farmService.getAll(pageable);
+        List<Farm> farms = farmsPage.getContent();
+
+        mv.addObject("farms", farms);
+
+        return mv;
+    }
+
+}
